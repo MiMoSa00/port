d59b990905d5bc93a461a4317ccea278b40b926f
MiMoSa.00 <umarkrishna558@.gmail.com>
2024-02-04 22:05:39 +0100

    reactportfolio

---
+++ src/Pages/Home/Homescreen/AboutMe.jsx
@@ -0,0 +1,21 @@
+export default function AboutMe() {
+    return (
+        <section id="AboutMe" className="about--section">
+       <div className="about--section--img">
+        <img src="./img/about-me.png" alt="About Me" />
+       </div>
+       <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+            <p className="section--title">About</p>
+            <h1 className="skills-section--heading">About Me</h1>
+            <p className="hero--section-description">Lorem ipsum dolor sit, amet consectetur 
+            adipisicing elit. Nemo recusandae ad quidem, sapiente vel iusto? Cupiditate,
+             perspiciatis voluptates excepturi 
+            vero quaerat eveniet similique vitae, obcaecati nesciunt nam repellendus atque. Vero!</p>
+            <p className="hero--section-description">Lorem ipsum dolor sit, amet consectetur 
+            adipisicing elit. Nemo recusandae ad quidem, sapiente vel iusto? Vero!</p>
+        </div>
+       </div>
+        </section>
+    )
+}
---
+++ src/Pages/Home/Homescreen/index.jsx
@@ -0,0 +1,24 @@
+import HeroSection from "../HeroSection";
+import MyPortfolio from "../MyPortfolio";
+import MySkills from "../MySkills";
+import Testimonial from "../Testimonials";
+import AboutMe from "./AboutMe";
+import ContactMe from "../ContactMe";
+import Footer from "../Footer";
+
+
+export default function Home () {
+
+return(
+    <>
+    <HeroSection />
+    <MySkills />
+    <AboutMe />
+    <MyPortfolio />
+    <Testimonial />
+    <ContactMe />
+    <Footer />
+
+    </>
+)
+}
